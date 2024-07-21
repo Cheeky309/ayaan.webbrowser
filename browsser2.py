f937feebdcdbf4088d6c31f0b6b4da75ed958509
Cheeky309 <ashaz.hasan@education.nsw.gov.au>
2024-07-21 13:58:54 +1000

    Update browsser2.py

--- browsser2.py
+++ browsser2.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import *   #pyqt5 is a browser library
+
 from PyQt5.QtGui import *
+
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
